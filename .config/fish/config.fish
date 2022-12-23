09f739951b5a872133586eb870a6a0ec3712c6be
Evan Lesmez <evan.lesmez@gmail.com>
2022-12-23 12:29:08 -0500

    Update fsh config for safer tmux start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-and not set -q TMUX
-    exec tmux
+	and xset q &>/dev/null 
+	and not set -q TMUX		
+	exec tmux
 end
+
 
 starship init fish | source
