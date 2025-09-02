ac6a0d32d518111a60f066c0f68d597c1269c257
Evan Lesmez <evan.lesmez@gmail.com>
2025-09-02 19:28:18 -0400

    Increase mouse sense because felt off on windows

--- .scripts/set-mouse-flat-accel.sh
+++ .scripts/set-mouse-flat-accel.sh
@@ -1,4 +1,4 @@
 POINTER_ID=$(xinput list | grep "BenQ ZOWIE BenQ ZOWIE Gaming Mouse" | grep "pointer" | grep -o 'id=[0-9]*' | cut -d'=' -f2)
 echo $POINTER_ID
 xinput set-prop $POINTER_ID "libinput Accel Profile Enabled" 0, 1, 0
-xinput set-prop $POINTER_ID "libinput Accel Speed" -.2
+xinput set-prop $POINTER_ID "libinput Accel Speed" -0.05
